7bc5d51c8653caadadfdef31d409dc62be6d4048
sanjana-user <sanjana.akella6@gmail.com>
2025-12-01 22:41:39 +0530

    Add files via upload

---
+++ rag.py
@@ -0,0 +1,142 @@
+import os
+print("RUNNING FILE:", os.path.abspath(__file__))
+
+
+print("RAG SCRIPT STARTED")
+
+OPENAI_API_KEY = "sk-proj-API_Key_sample"
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+print("Loading PDF files...")
+
+
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from uuid import uuid4
+
+# import the .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+# configuration
+DATA_PATH = r"data"
+CHROMA_PATH = r"chroma_db"
+
+# initiate the embeddings model
+embeddings_model = OpenAIEmbeddings(model="text-embedding-3-large")
+
+# initiate the vector store
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings_model,
+    persist_directory=CHROMA_PATH,
+)
+
+# loading the PDF document
+loader = PyPDFDirectoryLoader(DATA_PATH)
+
+raw_documents = loader.load()
+print("PDF LOADED")
+
+# splitting the document
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=100,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+# creating the chunks
+chunks = text_splitter.split_documents(raw_documents)
+print("CHUNKS CREATED")
+
+# creating unique ID's
+uuids = [str(uuid4()) for _ in range(len(chunks))]
+
+# adding chunks to vector store
+vector_store.add_documents(documents=chunks, ids=uuids)
+print("VECTORS STORED")
+
+
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_chroma import Chroma
+import gradio as gr
+
+# import the .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+# configuration
+DATA_PATH = r"data"
+CHROMA_PATH = r"chroma_db"
+
+embeddings_model = OpenAIEmbeddings(model="text-embedding-3-large")
+
+# initiate the model
+llm = ChatOpenAI(temperature=0.5, model='gpt-4o-mini')
+
+# connect to the chromadb
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings_model,
+    persist_directory=CHROMA_PATH, 
+)
+
+# Set up the vectorstore to be the retriever
+num_results = 5
+retriever = vector_store.as_retriever(search_kwargs={'k': num_results})
+
+# call this function for every message added to the chatbot
+def stream_response(message, history):
+    #print(f"Input: {message}. History: {history}\n")
+
+    # retrieve the relevant chunks based on the question asked
+    docs = retriever.invoke(message)
+
+    # add all the chunks to 'knowledge'
+    knowledge = ""
+
+    for doc in docs:
+        knowledge += doc.page_content+"\n\n"
+
+
+    # make the call to the LLM (including prompt)
+    if message is not None:
+
+        partial_message = ""
+
+        rag_prompt = f"""
+        You are an assistent which answers questions based on knowledge which is provided to you.
+        While answering, you don't use your internal knowledge, 
+        but solely the information in the "The knowledge" section.
+        You don't mention anything to the user about the povided knowledge.
+
+        The question: {message}
+
+        Conversation history: {history}
+
+        The knowledge: {knowledge}
+
+        """
+
+        #print(rag_prompt)
+
+        # stream the response to the Gradio App
+        for response in llm.stream(rag_prompt):
+            partial_message += response.content
+            yield partial_message
+
+# initiate the Gradio app
+chatbot = gr.ChatInterface(stream_response, textbox=gr.Textbox(placeholder="Send to the LLM...",
+    container=False,
+    autoscroll=True,
+    scale=7),
+)
+
+# launch the Gradio app
+print("STARTING GRADIO...")
+chatbot.launch()
+
